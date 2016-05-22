7fb45db02f4d22f2bd75f6fe44d0df737240d5f1
Tiago Espinha <tiago@espinha.nl>
2016-05-22 09:17:02 +0200

    Steps towards an official library release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject fun-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject org.tiago/fun-bot "1.0.0"
+  :description "fun-bot allows you to create a Slack bot which replies to the hooks you define"
+  :url "https://github.com/etiago/fun-bot"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.julienxx/clj-slack "0.5.4"]
                  [stylefruits/gniazdo "1.0.0"]
--- src/fun_bot/core.clj
+++ src/fun_bot/core.clj
@@ -9,41 +9,51 @@
         [slingshot.slingshot :only [throw+ try+]])
   (:gen-class))
 
-(def config
-  (try+
-   (edn/read-string (slurp "resources/config.edn"))
-   (catch Object _
-     (log/error "Config file (config.edn) not found!")
-     (System/exit -1))))
+(defn- send-message-to-slack
+  "Given a socket and an atom, send a text message to a Slack channel"
+  [socket msg-id-atom channel text]
+  (let [message-id @msg-id-atom]
+    (swap! msg-id-atom
+           (fn [current-id]
+             (inc current-id)))
+    (ws/send-msg socket
+                 (json/write-str {:id message-id
+                                  :type "message"
+                                  :channel channel
+                                  :text text}))))
 
-(def msg-id-atom (atom 0))
-
-(defn message-received
-  [socket message]
+(defn- message-received
+  "Callback function for websockets' client"
+  [socket-promise msg-id-atom trigger-cb-vector message]
   (if (not (nil? message))
     (let [message-kw (json/read-str message :key-fn keyword)]
-      (if (contains? message-kw :text)
-          (let [text (lower-case (get-in message-kw [:text]))]
-            (if (and (includes? text (get-in config [:bot-name]))
-                     (includes? text "tell me a joke"))
-              (let [message-id @msg-id-atom]
-                (swap! msg-id-atom
-                       (fn [current-id]
-                         (inc current-id)))
-                (ws/send-msg @socket
-                             (json/write-str {:id message-id
-                                              :type "message"
-                                              :channel (get-in message-kw [:channel])
-                                              :text "A joke!!"})))))))))
+      (if (and
+           (contains? message-kw :type)
+           (= "message"
+              (get-in message-kw [:type]))
+           (contains? message-kw :text))
+        (let [text (lower-case (get-in message-kw [:text]))]
+          (doseq [trigger-cb trigger-cb-vector]
+            (if ((nth trigger-cb 0) text)
+              (send-message-to-slack
+               @socket-promise
+               msg-id-atom
+               (get-in message-kw [:channel])
+               ((nth trigger-cb 1) text)))))))))
 
-(defn -main
-  [& args]
-  (let [socket-promise (promise)]
+;; API
+(defn start
+  [api-token trigger-cb-vector]
+  (let [socket-promise (promise)
+        msg-id-atom (atom 0)]
     (future
       (let [socket (ws/connect
                     (get-in (clj-slack.rtm/start
                              {:api-url "https://slack.com/api"
-                              :token (get-in config [:api-token])})
-                            [:url])
-                    :on-receive (partial message-received socket-promise))]
-        (deliver socket-promise socket)))))   
+                              :token api-token})
+                            [:url]) ;; Start the RTM session and get a websocket URL to connect to
+                    :on-receive (partial message-received
+                                         socket-promise
+                                         msg-id-atom
+                                         trigger-cb-vector))]
+        (deliver socket-promise socket)))))
